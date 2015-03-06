eec3550e2789bd68c4a0478cf96b451974c96ec5
tom-kav <kav.tommy@outlook.com>
2015-03-06 21:29:03 +0000

    Initial commit

---
+++ app/src/main/java/com/tommy/recyclerviewandcardview/ContactInfo.java
@@ -0,0 +1,11 @@
+package com.tommy.recyclerviewandcardview;
+
+public class ContactInfo {
+    protected String name;
+    protected String surname;
+    protected String number;
+
+    protected static final String NAME_PREFIX = "Name_";
+    protected static final String SURNAME_PREFIX = "Surname_";
+    protected static final String NUMBER = "123 45678910";
+}
---
+++ app/src/main/java/com/tommy/recyclerviewandcardview/MainActivity.java
@@ -0,0 +1,66 @@
+package com.tommy.recyclerviewandcardview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Using a RecyclerView because of its performance benefits vs. ListView
+        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        ContactAdapter ca = new ContactAdapter(createContactInfoList(10));
+        recList.setAdapter(ca);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<ContactInfo> createContactInfoList(int size) {
+
+        List<ContactInfo> list = new ArrayList<ContactInfo>();
+        for (int i=1; i <= size; i++) {
+            ContactInfo ci = new ContactInfo();
+            ci.name = ContactInfo.NAME_PREFIX + i;
+            ci.surname = ContactInfo.SURNAME_PREFIX + i;
+            ci.number = ContactInfo.NUMBER;
+
+            list.add(ci);
+        }
+
+        return list;
+    }
+}
